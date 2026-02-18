3b977e9e45ebbdad2397df6381bdedf26aee757c
Lars Sluijter <zegmaarlars@gmail.com>
2026-02-18 19:30:22 +0100

    Add basic datastore implementation (#1)
    * Add basic datastore implementation
    
    * Fix PR findings
    
    ---------
    
    Co-authored-by: Lars Sluijter <lha.sluijter@gmail.com>

---
+++ data/src/commonMain/kotlin/com/voxenlabs/dataStore/DataStore.kt
@@ -0,0 +1,36 @@
+package com.voxenlabs.dataStore
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+open class DataStore<K, V> {
+    private val mutex = Mutex()
+    private val cachedData = mutableMapOf<K, V>()
+
+    suspend fun add(
+        key: K,
+        value: V,
+    ) {
+        mutex.withLock {
+            cachedData.put(key, value)
+        }
+    }
+
+    suspend fun get(key: K): V? {
+        mutex.withLock {
+            return cachedData.get(key)
+        }
+    }
+
+    suspend fun remove(key: K) {
+        mutex.withLock {
+            cachedData.remove(key)
+        }
+    }
+
+    suspend fun clear() {
+        mutex.withLock {
+            cachedData.clear()
+        }
+    }
+}
